af6622d727a6a613a35c44564c8b747d80d469af
= <=>
2023-07-11 11:51:51 +0400

    fixes2

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import {ListGroupItem} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Collapse, ListGroupItem} from 'react-bootstrap';
 import MyButton from "./UI/button/MyButton";
 
 
 const PostItem = ({post,remove}) => {
+    const [open, setOpen] = useState(true);
     return (
-        <ListGroupItem className="d-flex w-100 justify-content-between align-items-start">
-            <div>
-                <h3>{post.id}. {post.name}</h3>
-                {post.body}
+        <Collapse
+            in={open}
+            dimension="height"
+            appear
+            onExited={() =>remove(post.id)}
+            // remove(post.id)
+        >
+            <div id={post.id}>
+                <ListGroupItem className="d-flex w-100 justify-content-between align-items-start">
+                    <div>
+                        <h3>{post.id}. {post.name}</h3>
+                        {post.body}
+                    </div>
+                    <div>
+                        <MyButton onClick={() =>{setOpen(false)}}
+                                  variant={"danger"}>Удалить</MyButton>
+                    </div>
+                </ListGroupItem>
             </div>
-            <div>
-                <MyButton onClick={() =>remove(post.id)}
-                    variant={"danger"}>Удалить</MyButton>
-            </div>
-        </ListGroupItem>
+        </Collapse>
+
     )
 }
 export default PostItem
